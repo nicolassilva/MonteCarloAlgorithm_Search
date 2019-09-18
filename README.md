e185b426429ea5d7667f93747a80e391f8a5cb0d
nicolasSilva <silva.nicolas.j@gmail.com>
2019-09-18 18:19:52 +0200

    All project update

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-#Nicolas Silva
+##Sujet: Repliement d'un modèle simplifié de protéine par un algorithme de Monte-Carlo et échange de répliques*
 
-*Sujet: Repliement d'un modèle simplifié de protéine par un algorithme de Monte-Carlo et échange de répliques*
+Nicolas Silva
+silva.nicolas.j@gmail.com
+Université Paris Diderot - Paris 7
+Septembre 2019 - Projet court
 
+__Objectif__
+
+Le but de ce projet est d'implémenter une algorithme de Monte Carlo afin d'effectuer des repliements de chaîne protéiques.
+Ce travail est basé sur l'article suivant :
+*Thachuk C, Shmygelska A, Hoos HH. A replica exchange Monte Carlo algorithm for protein folding in the HP model. BMC Bioinformatics. 2007 Sep 17;8:342. PubMed PMID: 17875212; PubMed Central PMCID: PMC2071922.*
+
+
+
+Exécution sous l'environnement python3
+
+#### Répertoires et Localisation des fichiers
+*********************************************
+
+Le répertoire contient :
+	- un dossier *data* contenant le fichier avec une séquence hydro/polaire d'acides aminés.
+	- un dossier *doc* contenant le rapport du projet.
+	- un dossier *results* contenant les résultats de repliements protéiques après l'algorithme de Monte Carlo.
+	- un dossier *src* contenant le script utilisé
+	- un fichier *.yml* permettant de recréer l'environnement conda utilisé
+
+#### Programme
+**************
+
+Il n'y a qu'un seul programme à exécuter: le programme monteCarlo_Search.py
+Ce programme prend 2 paramètres:
+	- le premier est le fichier contenant la séquence d'intérêt
+	- le second est le nombre de fois que l'algorithme de Monte Carlo doit être effectué.
+
+''' Ex: python3 monteCarlo_Search.py 500 '''
+
+#### Visualisation de la conformation
+*************************************
+
+La conformation finale obtenue après repliement est enregistrer dans un fichier results.txt
+
+#### Modules python importées
+*****************************
+
+Numpy, sys
--- src/monteCarlo_Search.py
+++ src/monteCarlo_Search.py
@@ -1,10 +1,16 @@
+###### Nicolas Silva
+###### Université Paris Diderot - Paris 7
+###### Septembre 2019 - Projet court
+
+#Import libraries
 import numpy as np
 from random import *
 import sys
 
-
+#Class creation to read the file containing the sequence
 class File():
 	def __init__(self, filename):
+		"""Input : __init__(filename)"""
 		self.filename = filename
 
 	def readFile(self):
@@ -12,8 +18,10 @@ class File():
 		fichier = open(self.filename,"r")
 		return fichier.readline().rstrip('\n')
 
+#Class création to create an array and calculate the energy of the conformation
 class Array():
 	def __init__(self, length):
+		"""Input : __init__(length)"""
 		self.length = length
 
 	def createArray(self):
@@ -23,7 +31,8 @@ class Array():
 		return mat
 
 	def energy(self,matrix,seq,dic):
-		"""Calculate the total energy of a conformation of amino acids"""
+		"""Calculate the total energy of a conformation of amino acids
+		Input : energy(matrix, sequence, dictionnary)"""
 		e = 0
 		for a in range(0,len(seq)):
 			if seq[a] != 'h':
@@ -34,27 +43,30 @@ class Array():
 					for l in range(j-1,j+2):
 						if matrix[k][l] == 'h':
 							e = e - 1
-				e = e + 1
+				e = e + 1 #As the code is counting the 'h' of all the nine positions, it is necessary to add one to the energy to count only the neighbors and not the 'h' itself
 		return e
 
 	def writeFile(self,matrix,filename):
-		"""Write data in file"""
+		"""Write data in file
+		Input : writeFile(matrix, filename)
+		Ouput : file"""
 		np.savetxt(filename, matrix, fmt='%-10.1c', delimiter='')
 		return
 
 
 if __name__ == '__main__':
-
+	#Verify if nstep is a positive integer
 	if (int(sys.argv[2]) < 1):
 		print('Le second argument doit être un entier positif')
 		sys.exit()
-	nstep = int(sys.argv[2])
+	nstep = int(sys.argv[2]) #Nstep as the second argument
 	
 	#Import the sequence
 	sequence = File(sys.argv[1])
 	seq = sequence.readFile()
 	#print(len(seq))
 
+	#Create a matrix of 2 times the length of the sequence
 	matrice = Array(len(seq))
 	mat = matrice.createArray()
 
@@ -73,7 +85,6 @@ if __name__ == '__main__':
 		dic[list_aa[i]] = (len(seq),j)
 		j=j+1
 	#print(dic)
-	#print(mat)
 
 	#Initial Energy calculation
 	Einit = matrice.energy(mat,seq,dic)
@@ -82,15 +93,14 @@ if __name__ == '__main__':
 	#Monte Carlo random algorithm
 	for k in range(nstep):
 		newDic = dic
-		a = randint(0,len(list_aa)-1)
-		(i,j) = newDic[a]
-		#print(i,j)
-		d = randint(1,8)
+		a = randint(0,len(list_aa)-1) #Choose a random amino acid to move
+		(i,j) = newDic[a] #Affect to i and j the coordinates of the amino acid
+		d = randint(1,8) #Choose randomly a move to do
 		if d==1: #Left moove
-			if (mat[i][j-1] != '.'):
+			if (mat[i][j-1] != '.'): #Verify if the move is possible
 				break
 			else:					
-				for l in range(a,len(list_aa)):
+				for l in range(a,len(list_aa)): #Move the amino acid and all the amino acids after it
 					(i,j) = newDic[l]
 					newDic[l] = (i,j-1)
 		elif d==2: #Right moove
